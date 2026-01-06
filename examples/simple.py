a8540e17535e4eada891a1aa264ccbfdeb28ffca
sina <20732540+SinaKhalili@users.noreply.github.com>
2026-01-05 16:20:33 -0800

    Update cargo, cargo lock

---
+++ examples/simple.py
@@ -0,0 +1,5 @@
+from driftpyrs import driftpy, get_vault_program_id
+
+if __name__ == "__main__":
+    print("Vault program ID:", get_vault_program_id())
+    print("driftpy version:", driftpy.__version__)
